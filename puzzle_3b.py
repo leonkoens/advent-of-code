3946d327d186f873dda3ffdbf13c6726dc1dffab
Leon Koens <info@leonkoens.nl>
2019-12-05 17:01:03 +0100

    Puzzle 3

--- puzzle_3b.py
+++ puzzle_3b.py
@@ -8,10 +8,11 @@ with open('input_3.txt', 'r') as handle:
 first = content[0]
 second = content[1]
 
-distance = 9999999
-
 locations = set()
 location = [0, 0]
+
+steps_dict = dict()
+steps = 0
 
 for update in first.split(','):
 
@@ -39,10 +40,21 @@ for update in first.split(','):
         location[0] += new_location[0]
         location[1] += new_location[1]
 
+        steps += 1
+
+        try:
+            if steps < steps_dict[tuple(location)]:
+                steps_dict[tuple(location)] = steps
+        except KeyError:
+            steps_dict[tuple(location)] = steps
+
         locations.add(tuple(location))
 
 
 location = [0, 0]
+steps = 0
+
+lowest = 999999999999
 
 for update in second.split(','):
 
@@ -70,7 +82,10 @@ for update in second.split(','):
         location[0] += new_location[0]
         location[1] += new_location[1]
 
-        if tuple(location) in locations:
-            distance = min(distance, abs(location[0]) + abs(location[1]))
+        steps += 1
 
-print(distance)
+        if tuple(location) in locations:
+
+            lowest = min(lowest, steps + steps_dict[tuple(location)])
+
+print(lowest)
